302184a23729c787eea74ddf9e0cf42e2e640f47
Rohit Gade <rohitgade@Rohits-MacBook-Pro.local>
2022-03-09 15:52:25 -0500

    changed screenshort scrupt

--- sh.js
+++ sh.js
@@ -1,8 +1,10 @@
 const puppeteer = require('puppeteer');
 async function run() {
+const browserFetcher = puppeteer.createBrowserFetcher();
+const revisionInfo = await browserFetcher.download('533271');
 let browser = await puppeteer.launch({ headless: false });
 let page = await browser.newPage();
-await page.goto('https://www.scrapehero.com/');
+await page.goto('https://www.google.com');
 await page.screenshot({ path: './imgs/image.jpg', type: 'jpeg' });
 await page.close();
 await browser.close();
